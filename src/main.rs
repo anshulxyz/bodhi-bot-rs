bec23a26ddf6f755139459d18e6478ed437c8492
Anshul Chauhan <anshulchauhan@outlook.com>
2022-01-05 02:22:42 +0530

    Add the commands framework

--- src/main.rs
+++ src/main.rs
@@ -2,39 +2,22 @@ use std::env;
 
 use serenity::{
     async_trait,
+    framework::{
+        standard::{
+            macros::{command, group},
+            Args, CommandResult,
+        },
+        StandardFramework,
+    },
     model::{channel::Message, gateway::Ready},
     prelude::*,
+    utils::MessageBuilder,
 };
 struct Handler;
 
 #[async_trait]
 impl EventHandler for Handler {
-    async fn message(&self, ctx: Context, msg: Message) {
-        if msg.content == "!msgme" {
-            if let Err(why) = msg.author.dm(&ctx, |m| m.content("Hello!")).await {
-                println!("Error when direct messaging user: {:?}", why);
-            }
-        }
-
-        if msg.content == "!ping" {
-            if let Err(why) = msg
-                .channel_id
-                .send_message(&ctx, |m| {
-                    m.embed(|e| {
-                        e.title("This is an embed");
-                        e.description("With a description");
-
-                        e
-                    });
-
-                    m
-                })
-                .await
-            {
-                println!("Error sending message: {:?}", why);
-            }
-        }
-    }
+    async fn message(&self, _ctx: Context, _msg: Message) {}
 
     async fn ready(&self, _: Context, ready: Ready) {
         println!("{} is connected!", ready.user.name);
@@ -48,12 +31,37 @@ async fn main() {
     // discord bot token
     let token = env::var("DISCORD_TOKEN").expect("Expected a token from environment.");
 
+    let framework = StandardFramework::new()
+        .configure(|f| {
+            f.with_whitespace(false)
+                .prefix("++")
+                .case_insensitivity(false)
+                .delimiters(vec![" "])
+        })
+        .group(&GENERAL_GROUP);
+
     // app client
     let mut client = Client::builder(&token)
         .event_handler(Handler)
+        .framework(framework)
         .await
         .expect("Error creating client");
     if let Err(why) = client.start().await {
         println!("Client error: {:?}", &why)
     }
+}
+
+#[group]
+#[commands(dhp)]
+struct General;
+
+#[command]
+async fn dhp(ctx: &Context, msg: &Message, args: Args) -> CommandResult {
+    let response = MessageBuilder::new()
+        .push("All arguments are: ")
+        .push(&args.rest())
+        .build();
+
+    msg.channel_id.say(&ctx.http, &response).await?;
+    Ok(())
 }
